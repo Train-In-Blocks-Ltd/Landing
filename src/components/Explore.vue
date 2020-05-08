4c0e940481ed2a1566c8a004ac1a027560f6efc4
Mikey Lau <mikey.lau@traininblocks.com>
2020-05-08 22:43:39 +0100

    Update Explore.vue

--- src/components/Explore.vue
+++ src/components/Explore.vue
@@ -3,6 +3,8 @@
     <h1 class="paper--title">{{title}}</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt sit ipsum architecto repellendus? Quo, molestias! Praesentium maiores ea in iste autem nisi, aliquid provident saepe, ipsa animi ad est! Fugit porro nihil error neque quisquam corrupti maiores perferendis ipsam molestiae. Mollitia officiis nesciunt, quam repudiandae magni deserunt nam illo quo?</p>
     <div class="spacer"></div>
+    <div id="curator-feed"><a href="https://curator.io" target="_blank" class="crt-logo crt-tag">Powered by Curator.io</a></div>
+    <div class="spacer"></div>
     <div class="container--2c">
       <div class="desc--subscribe">
         <h1>Get More From Our Emails</h1>
@@ -12,6 +14,15 @@
   </div>
 </template>
 
+<script type="text/javascript">
+  /* curator-feed */
+  (function(){
+    var i, e, d = document, s = "script";i = d.createElement("script");i.async = 1;
+    i.src = "https://cdn.curator.io/published/a66e0477-2159-494b-b9e5-ac8f8573ecdd.js";
+    e = d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
+  })();
+</script>
+  
 <script>
   import Subscribe from './Subscribe'
 
