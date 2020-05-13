31d4fc5cfb15fe886e1857436a4fb5bde46a5472
Mikey Lau <mikey.lau@traininblocks.com>
2020-05-13 15:11:59 +0100

    Home SVG update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,21 +9,11 @@
     <div class="spacer"></div>
     <div id="container--2c3r">
       <h1 class="text--right" style="grid-area: aT">We've taken exercise programming to the next level.</h1>
-      <svg style="grid-area: aI" width="300" height="300" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M68.0008 100L52.0001 54.0263H0L100 0L68.0008 100Z" fill="#282828">
-          <animateMotion dur="3000ms" repeatCount="indefinite"
-          path="M 20 20 L 45 35 L 20 20"/>
-        </path>
-        <line id="line1" x1="250" y1="0" x2="0" y2="240" stroke="#282828" stroke-width="3"><animateMotion dur="2500ms" repeatCount="indefinite" path="M 20 20 L 45 35 L 20 20"/></line>
-        <line id="line2" x1="100" y1="0" x2="0" y2="90" stroke="#282828" stroke-width="3"><animateMotion dur="2000ms" repeatCount="indefinite" path="M 20 20 L 45 35 L 20 20"/></line>
-        <line id="line3" x1="170" y1="0" x2="0" y2="160" stroke="#282828" stroke-width="3"><animateMotion dur="3000ms" repeatCount="indefinite" path="M 20 20 L 45 35 L 20 20"/></line>
-      </svg>
+      <inline-svg style="grid-area: aI" :src="require('../assets/svg/Arrow.svg')"/>
       <h1 style="grid-area: bT">Speed, simplicity and functionality is our philosophy.</h1>
-      <svg style="grid-area: bI" width="200" height="200" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path id="box" d="M 10 10 L 90 10 L 90 90 L 10 90 Z" stroke="#282828" stroke-width="8"/>
-      </svg>
-      <h1 class="text--right" style="grid-area: cT">Use features that you only need, save your money and expand.</h1>
-      <inline-svg class="icon--custom" style="grid-area: cI" :src="require('../assets/svg/Custom.svg')"/>
+      <inline-svg style="grid-area: bI" :src="require('../assets/svg/FlyArrow.svg')"/>
+      <h1 style="grid-area: cT">Use features that you only need, save your money and expand.</h1>
+      <inline-svg style="grid-area: cI" :src="require('../assets/svg/Custom.svg')"/>
     </div>
     <div class="spacer"></div>
   </div>
@@ -66,11 +56,13 @@
       'aI aT'
       'bT bI'
       'cI cT';
-    grid-gap: 10rem 1rem;
     height: auto
   }
   #container--2c3r > div {
     margin: 5vh 0
+  }
+  #container--2c3r > h1 {
+    margin: auto
   }
   .desc {
     cursor: default
@@ -84,42 +76,16 @@
     border: 2px solid #282828;
     color: white
   }
-  .text--right {
-    text-align: right
-  }
   .img {
     width: 100%
   }
   #laptop {
     width: 40vw
   }
-
-  /* SVG */
-  .icon--custom {
-    margin: auto
+  .text--right {
+    text-align: right
   }
 
-  /* SVG Anim */
-  #line1 {
-    stroke-dasharray: 80;
-    animation: dash 1300ms linear infinite
-  }
-  #line2 {
-    stroke-dasharray: 50;
-    animation: dash 2000ms linear infinite
-  }
-  #line3 {
-    stroke-dasharray: 100;
-    animation: dash 1500ms linear infinite
-  }
-  #box {
-    stroke-dasharray: 100;
-    animation: dash 4000ms linear infinite
-  }
-  #expand {
-    stroke-dasharray: 150;
-    animation: dash 5000ms cubic-bezier(.39, .575, .565, 1) infinite alternate
-  }
   @keyframes dash {
     to {
       stroke-dashoffset: -1000
