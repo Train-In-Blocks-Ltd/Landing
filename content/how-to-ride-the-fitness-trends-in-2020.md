de3f09a47c802eaaf04f02cd5ccd6af7554c09bd
Mikey Lau <mikey.lau@traininblocks.com>
2020-06-14 14:21:18 +0100

    patch

---
+++ content/how-to-ride-the-fitness-trends-in-2020.md
@@ -0,0 +1,12 @@
+---
+title: How to ride the fitness trends in 2020
+slug: how-to-ride-the-fitness-trends-in-2020
+img: /02/woman-indoor-working.jpg
+excerpt: Learn how to take advantage of current trends and grow your business.
+---
+
+# How to ride the fitness trends in 2020
+
+<br/><br/>
+
+TEXT
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <div id="container--2c">
+    <div id="container--1c">
       <h1 class="main-title">
         Save more time and get more clients.
       </h1>
-      <img id="img--DevicePC" src="../assets/DevicePC.png" alt="Device">
+      <img id="img--DevicePC" src="../assets/DevicePC.png" alt="Device PC">
     </div>
     <div class="spacer" />
     <div id="container--2c3r">
@@ -15,11 +15,11 @@
       <h1 class="text--right main-title" style="grid-area: bT">
         Speed, simplicity and functionality is our philosophy.
       </h1>
-      <inline-svg style="grid-area: bI" :src="require('../assets/svg/FlyArrow.svg')" />
+      <img id="img--deviceTablet" style="grid-area: bI" src="../assets/DeviceTablet.png" />
       <h1 class="main-title" style="grid-area: cT">
         Use features that you only need, save your money and expand.
       </h1>
-      <inline-svg id="svg--3" style="grid-area: cI" :src="require('../assets/svg/Custom.svg')" />
+      <inline-svg style="grid-area: cI" :src="require('../assets/svg/FlyArrow.svg')" />
     </div>
     <div class="spacer" />
   </div>
@@ -46,15 +46,14 @@ export default {
 
 <style scoped>
   /* Containers */
-  #container--2c {
+  #container--1c {
     display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 1rem;
+    grid-gap: 4rem;
     height: auto;
     width: 100%
   }
-  #container--2c > h1 {
-    margin: auto
+  #container--1c h1 {
+    text-align: center
   }
   #container--2c3r {
     display: grid;
@@ -63,7 +62,7 @@ export default {
       'aI aT'
       'bT bI'
       'cI cT';
-    grid-gap: 3rem 1rem;
+    grid-gap: 4rem;
     height: auto
   }
   #container--2c3r > div {
@@ -77,17 +76,23 @@ export default {
   #img--DevicePC {
     width: 100%
   }
+  #img--deviceTablet {
+    width: 40vw;
+    margin: auto
+  }
   .text--right {
     text-align: right
   }
   svg {
     margin: auto
   }
-  #svg--3 {
-    width: 30vw
-  }
 
   /* Responsiveness */
+  @media (max-width: 992px) {
+    .container--2c3r {
+      grid-gap: 2rem
+    }
+  }
   @media (max-width: 768px) {
     h1 {
       margin: 0
@@ -104,10 +109,8 @@ export default {
     #svg--3 {
       width: auto
     }
-  }
-  @media (max-width: 992px) {
-    .container--2c3r {
-      grid-gap: 2rem
+    #img--deviceTablet {
+      width: 90vw
     }
   }
   @media (max-width: 360px) {
