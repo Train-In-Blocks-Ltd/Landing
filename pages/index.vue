d5060e4fa3e8f43e05c78f0a92c2daf61b1cee71
MikeyJL <mikey.lau@traininblocks.com>
2020-06-25 01:07:38 +0100

    Better nav bar and layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,3 @@
-<template>
-  <div>
-    <div id="container--1c">
-      <h1 class="main-title">
-        Save more time and get more clients.
-      </h1>
-      <img id="img--DevicePC" src="../assets/DevicePC.png" alt="Device PC">
-    </div>
-    <div class="spacer" />
-    <div id="container--2c3r">
-      <h1 class="main-title" style="grid-area: aT">
-        We've taken exercise programming to the next level.
-      </h1>
-      <inline-svg style="grid-area: aI" :src="require('../assets/svg/Arrow.svg')" />
-      <h1 class="text--right main-title" style="grid-area: bT">
-        Speed, simplicity and functionality is our philosophy.
-      </h1>
-      <img id="img--deviceTablet" style="grid-area: bI" src="../assets/DeviceTablet.png">
-      <h1 class="main-title" style="grid-area: cT">
-        Use features that you only need, save your money and expand.
-      </h1>
-      <inline-svg style="grid-area: cI" :src="require('../assets/svg/FlyArrow.svg')" />
-    </div>
-    <div class="spacer" />
-  </div>
-</template>
-
-<script>
-import InlineSvg from 'vue-inline-svg'
-
-export default {
-  components: {
-    InlineSvg
-  },
-  data () {
-    return {
-      title: 'Want to train more clients effectively?'
-    }
-  },
-  mounted () {
-    this.$parent.$parent.metaHelper.title = 'Programming for Personal Trainers'
-    this.$parent.$parent.metaHelper.description = 'Over-delivering doesn\'t have to cost you. Impress your clients and help them reach their health and fitness goals.'
-  }
-}
-</script>
-
 <style scoped>
   /* Containers */
   #container--1c {
@@ -72,7 +26,24 @@ export default {
     margin: auto
   }
 
+  /* Countdown */
+  .countdown--wrapper {
+    display: grid;
+    margin: 4rem 0;
+    animation-delay: 600ms
+  }
+  .container--countdown {
+    letter-spacing: .4rem;
+    font-size: 1.5rem
+  }
+  .countdown--desc {
+    margin: 2rem 0
+  }
+
   /* Misc. */
+  .text--center {
+    text-align: center
+  }
   #img--DevicePC {
     width: 100%
   }
@@ -113,9 +84,86 @@ export default {
       width: 90vw
     }
   }
+  @media (max-width: 576px) {
+    /* Countdown */
+    .container--countdown h2 {
+      font-size: 1rem
+    }
+    .container--countdown p {
+      font-size: .8rem;
+      margin: .6rem 0
+    }
+  }
   @media (max-width: 360px) {
+    /* Containers */
+    .container--countdown p {
+      font-size: .5rem;
+      margin: .4rem 0
+    }
+
+    /* Misc */
     svg {
       width: 80%
     }
   }
 </style>
+
+<template>
+  <div>
+    <div id="container--1c">
+      <div class="countdown--wrapper">
+        <div class="container--countdown">
+          <h2 class="sub-title text--center">
+            01.08.20
+          </h2>
+          <Countdown end="August 1, 2020" />
+          <p class="countdown--desc text--center">
+            <b>BETA RELEASE</b>
+          </p>
+        </div>
+      </div>
+      <img id="img--DevicePC" src="../assets/DevicePC.png" alt="Device PC">
+    </div>
+    <div class="spacer" />
+    <h1 class="main-title center">
+      Save more time and get more clients.
+    </h1>
+    <div class="spacer" />
+    <div id="container--2c3r">
+      <h1 class="main-title" style="grid-area: aT">
+        We've taken exercise programming to the next level.
+      </h1>
+      <inline-svg style="grid-area: aI" :src="require('../assets/svg/Arrow.svg')" />
+      <h1 class="text--right main-title" style="grid-area: bT">
+        Speed, simplicity and functionality is our philosophy.
+      </h1>
+      <img id="img--deviceTablet" style="grid-area: bI" src="../assets/DeviceTablet.png">
+      <h1 class="main-title" style="grid-area: cT">
+        Use features that you only need, save your money and expand.
+      </h1>
+      <inline-svg style="grid-area: cI" :src="require('../assets/svg/FlyArrow.svg')" />
+    </div>
+    <div class="spacer" />
+  </div>
+</template>
+
+<script>
+import InlineSvg from 'vue-inline-svg'
+import Countdown from '../components/Countdown'
+
+export default {
+  components: {
+    InlineSvg,
+    Countdown
+  },
+  data () {
+    return {
+      title: 'Want to train more clients effectively?'
+    }
+  },
+  mounted () {
+    this.$parent.$parent.metaHelper.title = 'Programming for Personal Trainers'
+    this.$parent.$parent.metaHelper.description = 'Over-delivering doesn\'t have to cost you. Impress your clients and help them reach their health and fitness goals.'
+  }
+}
+</script>
--- pages/pricing.vue
+++ pages/pricing.vue
@@ -1,3 +1,118 @@
+<style scoped>
+  #pricing {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 1vw;
+    margin-top: 3rem
+  }
+  .pricing__plans {
+    display: grid;
+    grid-template-rows: 1fr .1fr;
+    height: auto;
+    min-height: fit-content;
+    box-shadow: 0 20px 40px 12px rgba(0, 0, 0, .05);
+    background-color: white;
+    padding: 6rem 2rem;
+    text-align: center
+  }
+  .pricing__plans > div > p {
+    margin: 0
+  }
+  .pricing__cost {
+    font-size: 4rem;
+    margin: 1rem 0
+  }
+  button.signUp {
+    width: 70%;
+    height: auto;
+    margin: auto;
+    padding: 1rem .5rem
+  }
+  h2 {
+    width: fit-content;
+    margin: auto
+  }
+  .subtitle {
+    margin: 0;
+    font-size: 2rem
+  }
+  .pro {
+    background-color: #282828;
+    color: white;
+    padding: 0 1rem
+  }
+
+  /* Competition */
+  .title--compare {
+    text-align: center
+  }
+  .container--comparison {
+    display: grid;
+    grid-template-columns: 1fr .4fr 1fr;
+    grid-gap: 4rem
+  }
+  .container--comparison h1 {
+    margin: auto
+  }
+  .container--comp {
+    display: grid
+  }
+  .list__name {
+    font-weight: bold;
+    font-size: 2rem;
+    margin: 1rem 0
+  }
+  .list__desc {
+    margin: 1rem 0;
+    list-style-type: square
+  }
+  .list--overview, .tib {
+    display: grid;
+    grid-template-columns: 44px 1fr;
+    grid-gap: 2rem
+  }
+  .icon {
+    height: 44px;
+    width: 44px;
+    margin: 1rem 0 0 0
+  }
+  .tib {
+    margin: 0 0 2rem 0
+  }
+
+  /* Other */
+  .icon--gif {
+    margin: 3rem;
+    border-radius: 5px
+  }
+  @media (max-width: 992px) {
+    #pricing {
+      grid-template-columns: 1fr;
+      margin: 0 0 4rem 0
+    }
+    .container--comparison {
+      grid-gap: 2rem
+    }
+  }
+  @media (max-width: 768px) {
+    .list__name {
+      font-size: 1.6rem
+    }
+    .title--compare {
+      text-align: left
+    }
+    .container--comparison {
+      grid-template: repeat(3, .2fr) / 1fr
+    }
+  }
+  @media (max-width: 576px) {
+    .pricing__plans {
+      background-color: transparent;
+      box-shadow: none
+    }
+  }
+</style>
+
 <template>
   <div>
     <h1 class="paper--title">
@@ -125,118 +240,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-  #pricing {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 1vw;
-    margin-top: 3rem
-  }
-  .pricing__plans {
-    display: grid;
-    grid-template-rows: 1fr .1fr;
-    height: auto;
-    min-height: fit-content;
-    box-shadow: 0 20px 40px 12px rgba(0, 0, 0, .05);
-    background-color: white;
-    padding: 6rem 2rem;
-    text-align: center
-  }
-  .pricing__plans > div > p {
-    margin: 0
-  }
-  .pricing__cost {
-    font-size: 4rem;
-    margin: 1rem 0
-  }
-  .signUp {
-    width: 70%;
-    height: auto;
-    margin: auto;
-    padding: 1rem .5rem
-  }
-  h2 {
-    width: fit-content;
-    margin: auto
-  }
-  .subtitle {
-    margin: 0;
-    font-size: 2rem
-  }
-  .pro {
-    background-color: #282828;
-    color: white;
-    padding: 0 1rem
-  }
-
-  /* Competition */
-  .title--compare {
-    text-align: center
-  }
-  .container--comparison {
-    display: grid;
-    grid-template-columns: 1fr .4fr 1fr;
-    grid-gap: 4rem
-  }
-  .container--comparison h1 {
-    margin: auto
-  }
-  .container--comp {
-    display: grid
-  }
-  .list__name {
-    font-weight: bold;
-    font-size: 2rem;
-    margin: 1rem 0
-  }
-  .list__desc {
-    margin: 1rem 0;
-    list-style-type: square
-  }
-  .list--overview, .tib {
-    display: grid;
-    grid-template-columns: 44px 1fr;
-    grid-gap: 2rem
-  }
-  .icon {
-    height: 44px;
-    width: 44px;
-    margin: 1rem 0 0 0
-  }
-  .tib {
-    margin: 0 0 2rem 0
-  }
-
-  /* Other */
-  .icon--gif {
-    margin: 3rem;
-    border-radius: 5px
-  }
-  @media (max-width: 992px) {
-    #pricing {
-      grid-template-columns: 1fr;
-      margin: 0 0 4rem 0
-    }
-    .container--comparison {
-      grid-gap: 2rem
-    }
-  }
-  @media (max-width: 768px) {
-    .list__name {
-      font-size: 1.6rem
-    }
-    .title--compare {
-      text-align: left
-    }
-    .container--comparison {
-      grid-template: repeat(3, .2fr) / 1fr
-    }
-  }
-  @media (max-width: 576px) {
-    .pricing__plans {
-      background-color: transparent;
-      box-shadow: none
-    }
-  }
-</style>
