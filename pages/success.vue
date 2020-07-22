8e5fb1adda50d276f01aa39fbbbb10da2699411b
Joe <joe@joebailey.xyz>
2020-07-22 18:14:04 +0100

    Added more to success page

--- pages/success.vue
+++ pages/success.vue
@@ -5,7 +5,12 @@
     </h1>
     <h2>What happens now?</h2>
     <ul>
-      <li>You will receive and email to finshing signing-up your account</li>
+      <li>
+        You will receive and email to finshing signing-up your account
+      </li>
+      <li style="font-size: .8rem">
+        It can take up to an hour for the email to arrive
+      </li>
     </ul>
   </div>
 </template>
