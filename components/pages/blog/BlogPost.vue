6c284e493295a8d6aa789c9d1c08e3d880d44c9f
Mikey Lau <mikey.lau@traininblocks.com>
2021-12-18 11:02:59 +0000

    fix(blog): index page redesigned and responsive

---
+++ components/pages/blog/BlogPost.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="mb-8 md:mb-0 md:flex">
+    <img
+      :src="require(`../../../assets/blog-img/${post.img}`)"
+      class="w-full mb-6 md:mb-0 md:w-1/3 md:mr-8 max-w-4xl"
+    />
+    <div class="w-full md:w-2/3 max-w-4xl">
+      <nuxt-link :to="`/blog/${post.slug}/`">
+        <txt type="subtitle" class="hover:opacity-60 transition-all">
+          {{ post.title }}
+        </txt>
+      </nuxt-link>
+      <txt class="my-4">
+        {{ post.postDesc }}
+      </txt>
+      <nuxt-link
+        class="flex items-center hover:opacity-60 transition-all"
+        :to="`/blog/${post.slug}/`"
+      >
+        <txt class="mr-4">Continue reading</txt>
+        <inline-svg :src="require('../../../assets/svg/Arrow.svg')" />
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import Txt from "../../elements/Txt";
+
+export default {
+  components: {
+    Txt,
+  },
+  props: {
+    post: {
+      type: {
+        title: String,
+        img: String,
+        postDesc: String,
+        slug: String,
+      },
+      default: {},
+    },
+  },
+};
+</script>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,109 +1,21 @@
-<style scoped>
-.blog_container {
-  display: grid;
-  grid-gap: 8rem;
-  margin: 4rem 0;
-}
-.blog_post {
-  display: grid;
-  grid-template-columns: 0.5fr 1fr;
-  grid-gap: 2rem;
-}
-.blog_post > img {
-  width: 100%;
-  max-width: 300px;
-  border-radius: 10px;
-  box-shadow: var(--low_shadow);
-  filter: grayscale(100%);
-  transition: var(--transition-standard);
-}
-.blog_post__content {
-  display: flex;
-  flex-direction: column;
-  padding-top: 2rem;
-}
-.blog_post__content a {
-  text-decoration: none;
-  font-weight: bold;
-  transition: opacity 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
-}
-.blog_post__content a:hover {
-  opacity: 0.6;
-}
-.blog_post__content a:active {
-  opacity: 0.4;
-}
-.blog_post__link {
-  display: grid;
-  grid-template-columns: 160px 24px;
-  grid-gap: 0.4rem;
-  margin: 2rem 0 0 0;
-  transition: grid-gap 0.4s cubic-bezier(0.165, 0.84, 0.44, 1);
-}
-.blog_post__link_text {
-  color: var(--base_dark);
-  margin: auto 0;
-}
-.blog_post__link:hover {
-  grid-gap: 1rem;
-}
-.focused_post img {
-  filter: grayscale(0);
-}
-
-/* Responsiveness */
-@media (max-width: 992px) {
-  .blog_post {
-    grid-template-columns: 0.8fr 1fr;
-  }
-  .blog_post__content {
-    padding: 0;
-  }
-  .blog_post__link:hover {
-    grid-gap: 0.4rem;
-    opacity: 1;
-  }
-}
-@media (max-width: 768px) {
-  .blog_post {
-    grid-template-columns: 1fr;
-    grid-gap: 2rem;
-  }
-}
-</style>
-
 <template>
   <div>
-    <h1>Find power in knowledge</h1>
-    <div class="blog_container">
-      <div v-for="post in posts" :key="post.title" class="blog_post">
-        <img :src="require(`../../assets/blog-img/${post.img}`)" />
-        <div class="blog_post__content">
-          <div>
-            <nuxt-link class="text--small" :to="`/blog/${post.slug}/`">
-              {{ post.title }}
-            </nuxt-link>
-            <p>
-              {{ post.postDesc }}
-            </p>
-          </div>
-          <div class="blog_post__link">
-            <nuxt-link class="blog_post__link_text" :to="`/blog/${post.slug}/`">
-              Continue reading
-            </nuxt-link>
-            <inline-svg
-              class="svg--read-more"
-              :src="require('../../assets/svg/Arrow.svg')"
-            />
-          </div>
-        </div>
-      </div>
+    <txt type="title" is-main class="mb-16">Find power in knowledge</txt>
+    <div class="grid gap-8 mb-8">
+      <blog-post v-for="post in posts" :key="post.title" :post="post" />
     </div>
   </div>
 </template>
 
 <script>
+import Txt from "../../components/elements/Txt";
+import BlogPost from "../../components/pages/blog/BlogPost";
+
 export default {
+  components: {
+    Txt,
+    BlogPost,
+  },
   async asyncData({ $content }) {
     const posts = await $content("blog").fetch();
     posts.sort((b, a) => {
