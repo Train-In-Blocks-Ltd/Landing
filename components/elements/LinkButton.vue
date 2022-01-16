607604899a8ee7c318108f7bd3c588468c9f47e3
Mikey Lau <mikey.lau@traininblocks.com>
2022-01-16 21:27:33 +0000

    fix(blog): button link press issue

--- components/elements/LinkButton.vue
+++ components/elements/LinkButton.vue
@@ -1,14 +1,11 @@
 <template>
-  <div
-    class="px-6 py-3 bg-gray-800 dark:bg-white rounded hover:opacity-60 transition-all w-fit"
-  >
-    <a
-      :href="link"
-      :target="newTab && '_blank'"
-      class="font-bold text-white dark:text-gray-800"
-      ><slot
-    /></a>
-  </div>
+  <a :href="link" :target="newTab && '_blank'">
+    <div
+      :class="`font-bold text-white dark:text-gray-800 px-6 py-3 bg-gray-800 dark:bg-white rounded hover:opacity-60 transition-all w-fit ${buttonClass}`"
+    >
+      <slot />
+    </div>
+  </a>
 </template>
 
 <script>
@@ -19,6 +16,7 @@ export default {
       default: "",
     },
     newTab: Boolean,
+    buttonClass: String,
   },
 };
 </script>
