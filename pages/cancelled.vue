a17b96da402b67a9002485bec29f16babd69547b
Joe <joe@joebailey.xyz>
2022-05-31 14:04:17 +0100

    Updated cancelled page with panel inner and testimonials

--- pages/cancelled.vue
+++ pages/cancelled.vue
@@ -1,24 +1,29 @@
 <template>
-  <div>
+  <page-wrapper id="cancelled">
     <txt type="title" is-main>Want to select a different plan?</txt>
     <txt type="large-body" class="mt-4 mb-16">
       Save big and spend less time managing your business and programmes.
     </txt>
     <payment class="mb-24" />
-    <benefits />
-  </div>
+    <benefits class="mb-24" />
+    <testimonials />
+  </page-wrapper>
 </template>
 
 <script>
+import PageWrapper from "../components/generic/PageWrapper";
 import Payment from "~/components/pages/Payment";
 import Benefits from "~/components/pages/Benefits";
 import Txt from "~/components/elements/Txt.vue";
+import Testimonials from "~/components/generic/Testimonials.vue";
 
 export default {
   components: {
+    PageWrapper,
     Txt,
     Payment,
     Benefits,
+    Testimonials,
   },
   head() {
     return {
