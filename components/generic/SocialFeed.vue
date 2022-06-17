c6302914880e7de135afe740b2f03ecba3cb8009
Mikey Lau <mikey.lau@traininblocks.com>
2022-06-17 20:38:48 +0100

    feat: IG feed

--- components/generic/SocialFeed.vue
+++ components/generic/SocialFeed.vue
@@ -1,30 +1,37 @@
+<style>
+.j-overlay-content {
+  @apply rounded-lg;
+}
+.j-overlay-content .j-meta {
+  @apply hidden;
+}
+.feed-item.juicer.image-post.juicer-about {
+  display: none;
+}
+h1.referral {
+  opacity: 0;
+}
+</style>
+
 <template>
   <div>
-    <txt type="title" class="mb-16 pt-8">Follow our journey</txt>
+    <txt type="title" class="pt-8">Follow our journey</txt>
 
-    <!-- Place <div> tag where you want the feed to appear -->
-    <div id="curator-feed-default-feed-layout">
-      <a href="https://curator.io" target="_blank" class="crt-logo crt-tag"
-        >Powered by Curator.io</a
-      >
-    </div>
-    <!-- The Javascript can be moved to the end of the html page before the </body> tag -->
-    <script type="text/javascript">
-      /* curator-feed-default-feed-layout */
-      (function () {
-        var i,
-          e,
-          d = document,
-          s = "script";
-        i = d.createElement("script");
-        i.async = 1;
-        i.charset = "UTF-8";
-        i.src =
-          "https://cdn.curator.io/published/a8874642-02a9-48a5-b88c-c4662403b7b3.js";
-        e = d.getElementsByTagName(s)[0];
-        e.parentNode.insertBefore(i, e);
-      })();
-    </script>
+    <script
+      src="https://assets.juicer.io/embed.js"
+      type="text/javascript"
+    ></script>
+    <link
+      href="https://assets.juicer.io/embed.css"
+      media="all"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <ul class="juicer-feed" data-feed-id="traininblocks">
+      <h1 class="referral">
+        <a href="https://www.juicer.io">Powered by Juicer.io</a>
+      </h1>
+    </ul>
   </div>
 </template>
 
