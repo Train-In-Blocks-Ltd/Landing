719de98173e4c772ea6da4168de02f7f8a63e7d1
Joe <joe@joebailey.xyz>
2022-06-27 19:29:11 +0100

    Fixed styling

--- components/generic/SocialFeed.vue
+++ components/generic/SocialFeed.vue
@@ -2,9 +2,6 @@
 .juicer-feed {
   margin-top: 1rem!important;
 }
-.j-overlay-content {
-  @apply rounded-lg;
-}
 .j-overlay-content .j-meta {
   @apply hidden;
 }
@@ -15,17 +12,25 @@
 .feed-item.juicer.image-post.juicer-about {
   @apply hidden;
 }
+.juicer-feed.image-grid li.feed-item {
+  aspect-ratio: 1;
+  overflow: hidden;
+  @apply rounded-lg;
+}
 .juicer-feed.image-grid li.feed-item:hover .j-text {
   @apply rounded-lg;
 }
-.juicer-feed h1.referral {
+.juicer-feed h1.referral, .juicer-feed li.feed-item:nth-of-type(n+2) {
   display: none!important
+}
+.juicer-feed.image-grid .j-text .j-message p, .juicer-feed.image-grid .j-text .j-edit p, .juicer-feed.slider .j-text .j-message p, .juicer-feed.slider .j-text .j-edit p, .juicer-feed.hip .j-text .j-message p, .juicer-feed.hip .j-text .j-edit p, .juicer-feed.image-grid .j-text .j-message a, .juicer-feed.image-grid .j-text .j-edit a, .juicer-feed.slider .j-text .j-message a, .juicer-feed.slider .j-text .j-edit a, .juicer-feed.hip .j-text .j-message a, .juicer-feed.hip .j-text .j-edit a {
+  color: white;
 }
 </style>
 
 <template>
   <div>
-    <txt type="title" class="pt-8">Follow our journey</txt>
+    <txt type="title" class="mb-16">Follow our journey</txt>
     <ul class="juicer-feed" data-feed-id="traininblocks" />
   </div>
 </template>
